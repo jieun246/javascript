1572fd39ad01e980cd98142e60c6ce1580f86c94
ije90 <86638467+ije90@users.noreply.github.com>
2021-06-30 15:04:47 +0900

    3강 완료
    3강 완료
    
    Co-Authored-By: jieun246 <79043826+jieun246@users.noreply.github.com>

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ function calculateKrAge(ageOfForeigner){
 /*============================================
 condition 
 ============================================*/
-const age = parseInt(prompt("How old are your?"));
+//const age = parseInt(prompt("How old are your?"));
 
 //console.log(isNaN(age));
 //if(isNaN(age)){
@@ -126,7 +126,7 @@ const age = parseInt(prompt("How old are your?"));
 //    console.log("Thank you for writing your age.");
 //}
 
-if(isNaN(age) || age < 0){
+/*if(isNaN(age) || age < 0){
     console.log("Please write a real positive Number");
 }else if(age < 18){
     console.log("You are too youg.");
@@ -136,4 +136,78 @@ if(isNaN(age) || age < 0){
     console.log("You should exercise");
 }else if(age > 80){
     console.log("You cand do whatever you want."); 
+}*/
+
+/*============================================
+The Document Object
+============================================*/
+//const title = document.getElementById("title"); 
+//title.innerText = "Got you!!";
+//console.dir(title);
+
+
+const title = document.querySelector(".hello h1");
+title.innerText = "Click Me";
+console.log(title);
+console.dir(title);
+
+function handleTitleClick(){
+    /*const currentColor = title.style.color;
+    let newColor;
+    if(currentColor === "blue"){
+        newColor="tomato";
+    }else{
+        newColor="blue";
+    }
+    title.style.color=newColor;*/
+
+    const activeClass = "active"; 
+    /*if(title.className ===activeClass){
+        title.className = ""; 
+    } else{
+        title.className =activeClass; 
+    }
+    if(title.classList.contains(activeClass)){
+        title.classList.remove(activeClass); 
+    } else{
+        title.classList.add(activeClass);
+    }*/
+    title.classList.toggle("active");
 }
+
+function handleMouseEnter(){
+    title.innerText="Mouse is here"; 
+}
+
+function handleMouseLeave(){
+    title.innerText="Mouse is gone"; 
+}
+
+function handleWindowResize(){
+    document.body.style.backgroundColor="tomato"; 
+}
+
+function handleWindowCopy(){
+    alert("coier");
+}
+
+function handleWindowOffline(){
+    alert("SOS no WIFI"); 
+}
+
+function handleWindowOnline(){
+    alert("WIFI on");
+}
+
+/*title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);*/
+title.onclick = handleTitleClick; 
+//title.onmouseenter = handleMouseEnter; 
+//title.onmouseleave = handleMouseLeave;
+
+//window.addEventListener("resize", handleWindowResize); 
+//window.addEventListener("copy", handleWindowCopy);
+//window.addEventListener("offline", handleWindowOffline);
+//window.addEventListener("online", handleWindowOnline);
+
